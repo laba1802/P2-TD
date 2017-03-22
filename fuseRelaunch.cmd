6dcc57444226459001a14e5a15b21f71c5c1baa2
laba1802 <laba1802@gmail.com>
2017-03-22 17:39:48 -0600

    Revert "Configuración sistema"
    This reverts commit cd43fc0edc6c51a432be1ff9f75768b205c47729.

--- fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "D:/Proyectos/Proyecto2Taller/state_machine_tb_isim_beh.exe" -prj "D:/Proyectos/Proyecto2Taller/state_machine_tb_beh.prj" "work.state_machine_tb" "work.glbl" 
