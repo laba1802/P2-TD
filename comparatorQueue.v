87a3c45d0334174ac19a2c1a8c1f45af8b14d098
laba1802 <laba1802@gmail.com>
2017-03-22 17:35:13 -0600

    Configuración sistema
    Se tiene la máquina de estados y la prueba para la máquina

---
+++ comparatorQueue.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    18:10:43 03/21/2017 
+// Design Name: 
+// Module Name:    comparatorQueue 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module comparatorQueue(
+    input [2:0] floorCall,
+    input [2:0] actualState,
+    input [2:0] firstPosMem,
+    input up_down_actual,
+    input up_down_call
+    );
+
+	//always @ (floorCall)
+
+endmodule
---
+++ fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "D:/Proyectos/Proyecto2Taller/state_machine_tb_isim_beh.exe" -prj "D:/Proyectos/Proyecto2Taller/state_machine_tb_beh.prj" "work.state_machine_tb" "work.glbl" 
